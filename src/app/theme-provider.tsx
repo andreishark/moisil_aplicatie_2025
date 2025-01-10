fe2edb00e7b653f1ecd4074291a791c21db8f30a
andreishark <andreishark10@gmail.com>
2025-01-10 22:09:31 +0200

    Deleted useless comment

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -7,9 +7,6 @@ export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  // if (!mounted) {
-  //   return <>{children}</>; // Render children without ThemeProvider during SSR
-  // }
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
