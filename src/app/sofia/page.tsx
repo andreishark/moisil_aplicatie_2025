6098f5306975960be0986fe422c7066d8b8f8f21
warlockina <sofia.anghelescu.ro@gmail.com>
2025-01-11 20:07:53 +0200

    Sofia

--- src/app/sofia/page.tsx
+++ src/app/sofia/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title:"Sofia's page"
+}
 
 export default function Home(){
     return(
-        <div className="grid grid-rows-6 items-center justify-items-stretch min-h-screen px-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-arial)]">
+        <div className="grid grid-rows-9 items-center justify-items-stretch min-h-screen px-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-arial)]">
             <h1 className="bg-purple-700 border-solid border-2 border-purple-500 rounded text-center text-lg italic font-medium">Hello</h1>
             <div className="flex flex-row">
                 <div className = "bg-indigo-500 border-dashed hover:border-solid border-4 border-slate-300 rounded-full text-center text-xl font-extrabold basis-1/6">
@@ -36,7 +41,6 @@ export default function Home(){
                       height={30}
             />Begin your journey
             </Link>
-            
         </div>
     );
 
