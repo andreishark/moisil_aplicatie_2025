a329ddd6b7a55a36e610d16cc181648afea9bd50
warlockina <sofia.anghelescu.ro@gmail.com>
2025-01-11 18:32:02 +0200

    Sofia folder

--- src/app/sofia/page.tsx
+++ src/app/sofia/page.tsx
@@ -2,19 +2,22 @@ import Image from "next/image";
 
 export default function Home(){
     return(
-        <div className="grid grid-rows-4 items-center justify-items-stretch min-h-screen px-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-arial)]">
-            <h1>Hello</h1>
+        <div className="grid grid-rows-5 items-center justify-items-stretch min-h-screen px-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-arial)]">
+            <h1 className="bg-purple-700 border-solid border-2 border-purple-500 rounded text-center text-lg italic font-medium">Hello</h1>
             <div className="flex flex-row">
-                <div className = "basis-1/6">a</div>
-                <div className = "basis-1/3">b</div>
-                <div className = "basis-1/2">c</div>
+                <div className = "bg-indigo-500 border-dashed hover:border-solid border-4 border-slate-300 rounded-full text-center text-xl font-extrabold basis-1/6">
+                    a</div>
+                <div className = "bg-violet-700 border-dashed hover:border-solid border-4 border-slate-300 rounded-full text-center text-xl font-bold basis-1/3">
+                    b</div>
+                <div className = "bg-fuchsia-500 border-dashed hover:border-solid border-4 border-slate-300 rounded-full text-center text-xl font-semibold basis-1/2">
+                    c</div>
             </div>
             <Image
                 className="dark:invert"
                 src="/globe.svg"
                 alt="Globe icon"
-                width={50}
-                height={60}
+                width={100}
+                height={100}
             />
             <p>This was made by <b>Sofia</b></p>
             <a
@@ -27,8 +30,8 @@ export default function Home(){
                       className="dark:invert"
                       src="/vercel.svg"
                       alt="Vercel logomark"
-                      width={20}
-                      height={20}
+                      width={30}
+                      height={30}
             />Begin your journey
             </a>
         </div>
