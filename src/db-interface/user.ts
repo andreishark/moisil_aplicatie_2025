032e73a40c96f2ef81321f7a40d51e38c02eb0a0
andreishark <andreishark10@gmail.com>
2025-01-13 00:36:32 +0200

    Temporary disable

--- src/db-interface/user.ts
+++ src/db-interface/user.ts
@@ -1,10 +1,10 @@
-import { prisma } from "./db";
+// import { prisma } from "./db";
 
-export default async function prismaExample() {
-  await prisma.user.create({
-    data: {
-      name: "Elliott",
-      email: "xelliottx@example-user.com",
-    },
-  });
-}
+// export default async function prismaExample() {
+//   await prisma.user.create({
+//     data: {
+//       name: "Elliott",
+//       email: "xelliottx@example-user.com",
+//     },
+//   });
+// }
