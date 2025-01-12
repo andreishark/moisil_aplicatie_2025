d44e9b9ad475447c702e270e632490499071ee15
BlueskyBV <alexcolojoara007@gmail.com>
2025-01-12 10:10:11 +0200

    Carousel for starting page

---
+++ src/app/Hp_Alex/page.tsx
@@ -0,0 +1,21 @@
+import { Metadata } from "next"
+import { CarouselStart } from "./Carousel"
+
+
+export const metadata: Metadata = {
+  title: "About us!",
+  description: "This is DedSec!"
+}
+
+export default function home()
+{
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 text-primary">
+      <header className="flex flex-col items-center gap-4">
+      </header>
+      <main className="flex flex-col items-center gap-6 mt-10">
+        <CarouselStart/>
+      </main>
+    </div>
+  )
+}
