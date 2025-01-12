d7411c733a126ac1fac9d4e4d58ac5f2df4f5693
BlueskyBV <alexcolojoara007@gmail.com>
2025-01-12 20:16:19 +0200

    Funny update

---
+++ src/app/Hp_Alex/Like-button.tsx
@@ -0,0 +1,17 @@
+'use client'; // Ensures client-side rendering
+
+import { useState } from 'react';
+
+export default function Liking() {
+  const [likes, setLikes] = useState(0);
+  
+  return (
+    <div>
+        <button
+      onClick={() => setLikes(likes + 1)} // Increment state on click
+    >
+      Drop a like! <br/> Likes: {likes}
+    </button>
+    </div>
+  );
+}
--- src/app/Hp_Alex/page.tsx
+++ src/app/Hp_Alex/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next"
 import { CarouselStart } from "./Carousel"
 import { DropdownMenuLogin } from "./dropdown_menu"
+import Liking from "./Like-button"
 
 
 export const metadata: Metadata = {
@@ -12,7 +13,7 @@ export default function home()
 {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 text-primary">
-      <header className="flex flex-col items-center gap-4">
+      <header>
       </header>
       <main className="flex flex-col items-center gap-6 mt-10">
         <CarouselStart/>
