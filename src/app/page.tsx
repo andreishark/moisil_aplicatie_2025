739872aa6def1f1734a8012c3942f1ba4141e3c7
andreishark <andreishark10@gmail.com>
2025-01-13 12:34:48 +0200

    Added data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
+import { getForestPercentPerYear } from "@/datasets/dataProcessing";
 import Image from "next/image";
 
-
 export default function Home() {
+  console.log(getForestPercentPerYear());
+
   return (
-     <div className="grid grid-rows-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Image
-        src="/nasa-earth.jpg"
-        alt="Earth photo"
-        fill={true}
-      />
-      <h1 className="text-6xl text-center font-semibold z-10 drop-shadow-2xl">Poți face o schimbare.</h1>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">        
-            
-      </main>
+    <div className="grid grid-rows-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Image src="/nasa-earth.jpg" alt="Earth photo" fill={true} />
+      <h1 className="text-6xl text-center font-semibold z-10 drop-shadow-2xl">
+        Poți face o schimbare.
+      </h1>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
     </div>
   );
 }
