c1b9080ff38473f88886dfccff4cff6ed2997b6f
warlockina <sofia.anghelescu.ro@gmail.com>
2025-01-13 12:39:41 +0200

    Main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Component } from  "@/components/ui/line-chart";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,15 +12,14 @@ export default function Home() {
       />
       <h1 className="mb-36 bg-clip-border border-slate-300 rounded-xl p-2 bg-gradient-to-r from-teal-500 to-indigo-600 text-6xl text-center font-semibold z-10 drop-shadow-2xl">Poți face o schimbare.</h1>
       </div>
-
+      <Link className="text-sm underline justify-self-start text-slate-300 z-10"
+            href="https://images.nasa.gov/details/iss036e005964"
+      >Poză: Nasa - Earth Observation </Link>
       <article className="mt-28 mx-24 z-10 bg-slate-700 p-3">
         <h2 className="text-lg font-semibold">Pentru o planetă mai sănătoasă</h2>
         <p className="text-wrap">A growing global population and economic shift towards more resource-intensive consumption patterns means that global freshwater use has increased approximately six-fold since 1900. Freshwater use includes freshwater withdrawals for agriculture, industry, and municipal uses. This is shown in the chart. Rates of global freshwater use increased sharply from the 1950s onwards but since 2000 appear to be plateauing, or at least slowing.</p>
       </article>
 
-      <div className="grid grid-flow-row items-center justify-items-stretch min-h-screen mx-36">
-        <Component/>
-      </div>
     </div>
   );
 }
