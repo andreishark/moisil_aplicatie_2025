c5e028af4d2186257bf21798c919ddef52af4088
BlueskyBV <alexcolojoara007@gmail.com>
2025-01-13 13:57:17 +0200

    .

--- src/app/Map/page.tsx
+++ src/app/Map/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import MapInteractiv from "./map_interactiv";
+
+export const metadata: Metadata = {
+  title: "Pagina principală",
+  description: "Aici este pagina noastra principală!",
+};
 
 export default function Page() {
   return (
