237b4c5f3f789c48c443308f91d94920babe0bd2
andreishark <andreishark10@gmail.com>
2025-01-13 13:20:01 +0200

    Added processing to map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,35 +8,40 @@ export default function Home() {
   return (
     <div className="grid grid-rows-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="grid grid-flow-row items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
         <div className="grid grid-flow-row items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <div>
             <Image
               src="/nasa-earth.jpg"
               alt="Earth photo"
               fill={true}
+              className="pt-[6rem]"
             />
-          <h1 className="mb-36 bg-clip-border border-slate-300 rounded-xl p-2 bg-gradient-to-r from-teal-500 to-indigo-600 text-6xl text-center font-semibold z-10 drop-shadow-2xl">
-            Poți face o schimbare.</h1>
+            <h1 className="md:mb-36 bg-clip-border border-slate-300 rounded-xl p-0 md:p-2 bg-gradient-to-r from-teal-500 to-indigo-600 text-6xl text-center font-semibold z-10 drop-shadow-2xl">
+              Poți face o schimbare.
+            </h1>
           </div>
-        
-          <Link className="text-sm underline justify-self-start text-slate-300 z-10"
-              href="https://images.nasa.gov/details/iss036e005964"
-          >Poză: Nasa - Earth Observation </Link>
-        
+
+          <Link
+            className="text-sm underline justify-self-start text-slate-300 z-10"
+            href="https://images.nasa.gov/details/iss036e005964"
+          >
+            Poză: Nasa - Earth Observation{" "}
+          </Link>
+
           <article className="mt-28 mx-24 z-10 bg-slate-700 p-3">
-          <h2 className="text-lg font-semibold">Pentru o planetă mai sănătoasă</h2>
-          <p className="text-wrap">
+            <h2 className="text-lg font-semibold">
+              Pentru o planetă mai sănătoasă
+            </h2>
+            <p className="text-wrap">
               A growing global population and economic shift towards more
-              resource-intensive consumption patterns means that global freshwater
-              use has increased approximately six-fold since 1900. Freshwater use
-              includes freshwater withdrawals for agriculture, industry, and
-              municipal uses. This is shown in the chart. Rates of global
-              freshwater use increased sharply from the 1950s onwards but since
-              2000 appear to be plateauing, or at least slowing.
+              resource-intensive consumption patterns means that global
+              freshwater use has increased approximately six-fold since 1900.
+              Freshwater use includes freshwater withdrawals for agriculture,
+              industry, and municipal uses. This is shown in the chart. Rates of
+              global freshwater use increased sharply from the 1950s onwards but
+              since 2000 appear to be plateauing, or at least slowing.
             </p>
           </article>
-
         </div>
       </div>
     </div>
--- src/datasets/dataProcessing.ts
+++ src/datasets/dataProcessing.ts
@@ -1,6 +1,6 @@
-import fs from "fs";
-import path from "path";
-import { parse } from "csv-parse";
+// import fs from "fs";
+// import path from "path";
+// import { parse } from "csv-parse";
 
 const currentYear = 2023;
 
@@ -15,76 +15,76 @@ for (let index = 1960; index <= currentYear; index++) {
   headers.push(index.toString());
 }
 
-type WorldData = {
-  "Country Name": string;
-  "Country Code": string;
-  "Indicator Name": string;
-  "Indicator Code": string;
-  1960: string;
-  1961: string;
-  1962: string;
-  1963: string;
-  1964: string;
-  1965: string;
-  1966: string;
-  1967: string;
-  1968: string;
-  1969: string;
-  1970: string;
-  1971: string;
-  1972: string;
-  1973: string;
-  1974: string;
-  1975: string;
-  1976: string;
-  1977: string;
-  1978: string;
-  1979: string;
-  1980: string;
-  1981: string;
-  1982: string;
-  1983: string;
-  1984: string;
-  1985: string;
-  1986: string;
-  1987: string;
-  1988: string;
-  1989: string;
-  1990: string;
-  1991: string;
-  1992: string;
-  1993: string;
-  1994: string;
-  1995: string;
-  1996: string;
-  1997: string;
-  1998: string;
-  1999: string;
-  2000: string;
-  2001: string;
-  2002: string;
-  2003: string;
-  2004: string;
-  2005: string;
-  2006: string;
-  2007: string;
-  2008: string;
-  2009: string;
-  2010: string;
-  2011: string;
-  2012: string;
-  2013: string;
-  2014: string;
-  2015: string;
-  2016: string;
-  2017: string;
-  2018: string;
-  2019: string;
-  2020: string;
-  2021: string;
-  2022: string;
-  2023: string;
-};
+// type WorldData = {
+//   "Country Name": string;
+//   "Country Code": string;
+//   "Indicator Name": string;
+//   "Indicator Code": string;
+//   1960: string;
+//   1961: string;
+//   1962: string;
+//   1963: string;
+//   1964: string;
+//   1965: string;
+//   1966: string;
+//   1967: string;
+//   1968: string;
+//   1969: string;
+//   1970: string;
+//   1971: string;
+//   1972: string;
+//   1973: string;
+//   1974: string;
+//   1975: string;
+//   1976: string;
+//   1977: string;
+//   1978: string;
+//   1979: string;
+//   1980: string;
+//   1981: string;
+//   1982: string;
+//   1983: string;
+//   1984: string;
+//   1985: string;
+//   1986: string;
+//   1987: string;
+//   1988: string;
+//   1989: string;
+//   1990: string;
+//   1991: string;
+//   1992: string;
+//   1993: string;
+//   1994: string;
+//   1995: string;
+//   1996: string;
+//   1997: string;
+//   1998: string;
+//   1999: string;
+//   2000: string;
+//   2001: string;
+//   2002: string;
+//   2003: string;
+//   2004: string;
+//   2005: string;
+//   2006: string;
+//   2007: string;
+//   2008: string;
+//   2009: string;
+//   2010: string;
+//   2011: string;
+//   2012: string;
+//   2013: string;
+//   2014: string;
+//   2015: string;
+//   2016: string;
+//   2017: string;
+//   2018: string;
+//   2019: string;
+//   2020: string;
+//   2021: string;
+//   2022: string;
+//   2023: string;
+// };
 
 type WorldDataCurated = {
   "Country Name": string;
@@ -96,38 +96,38 @@ type WorldDataCurated = {
 
 const results: WorldDataCurated[] = [];
 
-const csvFilePath = path.resolve(process.cwd(), "src/datasets/WDICSV.csv");
-
-const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
-
-parse(
-  fileContent,
-  {
-    delimiter: ",",
-    columns: headers,
-  },
-  (error, result: WorldData[]) => {
-    if (error) {
-      console.error(error);
-    }
-
-    for (let index = 0; index < result.length; index++) {
-      const yearReport = [];
-      for (let j = 1960; j <= currentYear; j++) {
-        //@ts-expect-error Indexing dynamically
-        yearReport.push(result[index][j]);
-      }
-
-      results.push({
-        "Country Name": result[index]["Country Name"],
-        "Country Code": result[index]["Country Code"],
-        "Indicator Name": result[index]["Indicator Name"],
-        "Indicator Code": result[index]["Indicator Code"],
-        data: yearReport,
-      });
-    }
-  },
-);
+// const csvFilePath = path.resolve(process.cwd(), "src/datasets/WDICSV.csv");
+//
+// const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
+//
+// parse(
+//   fileContent,
+//   {
+//     delimiter: ",",
+//     columns: headers,
+//   },
+//   (error, result: WorldData[]) => {
+//     if (error) {
+//       console.error(error);
+//     }
+//
+//     for (let index = 0; index < result.length; index++) {
+//       const yearReport = [];
+//       for (let j = 1960; j <= currentYear; j++) {
+//         //@ts-expect-error Indexing dynamically
+//         yearReport.push(result[index][j]);
+//       }
+//
+//       results.push({
+//         "Country Name": result[index]["Country Name"],
+//         "Country Code": result[index]["Country Code"],
+//         "Indicator Name": result[index]["Indicator Name"],
+//         "Indicator Code": result[index]["Indicator Code"],
+//         data: yearReport,
+//       });
+//     }
+//   },
+// );
 
 // const finalData = results
 //   .filter((val) => val["Indicator Name"] == "Forest area (% of land area)")
@@ -140,20 +140,20 @@ parse(
 
 // fs.writefile("forests_percent.json", json.stringify(finaldata), (err) => {});
 
-export function getForestPercentPerYear() {
-  const perYear = results
-    .filter(
-      (val) =>
-        val["Indicator Name"] == "Fossil fuel energy consumption (% of total)",
-    )
-    .map((val) => {
-      return {
-        countryCode: val["Country Code"],
-        data: val.data,
-      };
-    });
-
-  fs.writeFile("fuel.json", JSON.stringify(perYear), (err) => {});
-
-  return perYear;
-}
+// export function getForestPercentPerYear() {
+//   const perYear = results
+//     .filter(
+//       (val) =>
+//         val["Indicator Name"] == "Fossil fuel energy consumption (% of total)",
+//     )
+//     .map((val) => {
+//       return {
+//         countryCode: val["Country Code"],
+//         data: val.data,
+//       };
+//     });
+//
+//   fs.writeFile("fuel.json", JSON.stringify(perYear), (err) => {});
+//
+//   return perYear;
+// }
