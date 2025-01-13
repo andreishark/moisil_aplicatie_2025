8e53ea853035fc0cba5e4feed6672cdcc10e1af2
warlockina <sofia.anghelescu.ro@gmail.com>
2025-01-13 11:20:41 +0200

    Main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <div className="grid grid-rows-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     <div className="grid grid-flow-row items-center justify-items-center min-h-screen bg-sky-950 p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Image
         src="/nasa-earth.jpg"
         alt="Earth photo"
         fill={true}
       />
-      <h1 className="text-6xl text-center font-semibold z-10 drop-shadow-2xl">Poți face o schimbare.</h1>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">        
-            
-      </main>
+      <h1 className="bg-clip-border border-slate-300 rounded-md p-2 bg-gradient-to-r from-teal-500 to-indigo-600 text-6xl text-center font-semibold z-10 drop-shadow-2xl">Poți face o schimbare.</h1>
     </div>
   );
 }
